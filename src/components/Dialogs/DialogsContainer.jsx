a969a9e13503182959b7818522882673e7db7f74
Danichp <danichp01@mail.ru>
2023-01-25 21:48:46 +0700

    46

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -1,25 +1,26 @@
 import React from "react";
-import DialogItem from "./DialogItem/DialogItem";
-import Message from "./Message/Message";
+import { connect } from "react-redux";
 import { addMessageCreator, updateNewSendTextCreator } from "./../../redux/dialogsReducer";
 import Dialogs from "./Dialogs";
 
-
-const DialogsContainer = (props) => {
-
-    let state = props.store.getState().dialogsPage;
-
-
-    let addMessage = () => {
-        props.store.dispatch(addMessageCreator());
+let mapStateToProps = (state) => {
+    return {
+        dialogsPage: state.dialogsPage
     }
-
-    let onSendChange = (text) => {
-        props.store.dispatch(updateNewSendTextCreator(text));
-    }
-
-    return <Dialogs updateNewSendText={onSendChange} addMessage={addMessage} dialogsPage={state}/>
 }
+
+let mapDispatchToProps = (dispatch) => {
+    return {
+        updateNewSendText: (text) => {
+            dispatch(updateNewSendTextCreator(text));
+        },
+        addMessage: () => {
+            dispatch(addMessageCreator());
+        },
+    }
+}
+
+let DialogsContainer = connect(mapStateToProps, mapDispatchToProps) (Dialogs);
 
 
 export default DialogsContainer; 
--- src/components/Profile/MyPosts/MyPostContainer.jsx
+++ src/components/Profile/MyPosts/MyPostContainer.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { connect } from "react-redux";
 import { addPostActionCreator, updateNewPostTextActionCreator } from "../../../redux/profileReducer";
 import MyPost from "./MyPost";
 
 
 
 
-const MyPostsContainer = (props) => {
+const MyPostsContainerf = (props) => {
    
 
     let state = props.store.getState();
@@ -22,6 +23,33 @@ const MyPostsContainer = (props) => {
 
     return (< MyPost  updateNewPostText = {onPostChange}  addPost = {addPost} posts={state.profilePage.postData}
         newPostText={state.profilePage.newPostText}/>)
+
+
+
+    
+
 }
 
-export default MyPostsContainer;
+let mapStateToProps = (state) => {
+    debugger
+    return{
+        posts: state.profilePage.postData,
+        newPostText: state.profilePage.newPostText
+    }
+}
+let mapDispatchToProps = (dispatch) => {
+    return{
+        updateNewPostText: (text) => {
+            let action = updateNewPostTextActionCreator(text);
+            dispatch(action);
+        },
+        addPost: () => {
+            dispatch(addPostActionCreator());
+        }
+    }
+}
+
+
+const MyPostContainer = connect(mapStateToProps, mapDispatchToProps)(MyPost)
+
+export default MyPostContainer;
